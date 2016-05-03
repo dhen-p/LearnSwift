c58e8288d4f2b8d6d723274b7615c14a68f5804c
danny.p <danny.p@kakaocorp.com>
2016-05-03 10:31:21 +0900

    8. Swift And CocoaTouch

---
+++ SwiftAndCocoaTouch.playground/Contents.swift
@@ -0,0 +1,15 @@
+/***
+ * Enumerations and Structures
+ * Reference: https://developer.apple.com/library/ios/referencelibrary/GettingStarted/DevelopiOSAppsSwift/Lesson1.html#//apple_ref/doc/uid/TP40015214-CH3-SW1
+ * written by dhen-p
+ * 2016. 05. 03
+ **/
+
+import UIKit
+
+var str = "Hello, playground"
+
+let sampleString: String = "hello"
+let sampleArray: Array = [1, 2, 3.141592, 23, 42]
+let redSquare = UIView(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
+redSquare.backgroundColor = UIColor.redColor()
